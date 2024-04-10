435161af35b2a652aa721d2aa2f080a003196f75
santiago2389 <santoshkamble20@yahoo.com>
2024-04-10 16:57:15 +0530

    install dependencies added

--- template.py
+++ template.py
@@ -5,7 +5,7 @@ import logging
 logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
 
 
-project_name = "yolov8_obj_det"
+project_name = "YoloDetector"
 
 list_of_files = [
     ".github/workflows/.gitkeep",
